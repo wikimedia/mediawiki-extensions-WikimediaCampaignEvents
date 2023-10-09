b300bd02bfb6b001cbac4384e9edc789626e0da3
Claudio Melo <cmelo@wikimedia.org>
2023-10-09 10:04:08 -0300

    Add listener for the submit event data hook
    Bug: T346957
    Change-Id: Id933f5dfed43196ac8cc0a086408047199280ad2

--- src/Hooks/Handlers/EventRegistrationFormHandler.php
+++ src/Hooks/Handlers/EventRegistrationFormHandler.php
@@ -5,14 +5,17 @@ declare( strict_types=1 );
 namespace MediaWiki\Extension\WikimediaCampaignEvents\Hooks\Handlers;
 
 use MediaWiki\Extension\CampaignEvents\Hooks\CampaignEventsRegistrationFormLoadHook;
+use MediaWiki\Extension\CampaignEvents\Hooks\CampaignEventsRegistrationFormSubmitHook;
 use MediaWiki\Extension\CampaignEvents\Special\AbstractEventRegistrationSpecialPage;
+use StatusValue;
 
-class EventRegistrationFormHandler implements CampaignEventsRegistrationFormLoadHook {
+class EventRegistrationFormHandler implements
+	CampaignEventsRegistrationFormLoadHook,
+	CampaignEventsRegistrationFormSubmitHook
+{
 
 	/**
-	 * @param array &$formFields
-	 * @param int|null $eventID
-	 * @return true
+	 * @inheritDoc
 	 */
 	public function onCampaignEventsRegistrationFormLoad( array &$formFields, ?int $eventID ) {
 		// TDB get the grandID if it exists and set as the default value
@@ -22,9 +25,25 @@ class EventRegistrationFormHandler implements CampaignEventsRegistrationFormLoad
 			'default' => '',
 			'placeholder-message' => 'wikimediacampaignevents-grant-id-input-placeholder',
 			'help-message' => 'wikimediacampaignevents-grant-id-input-help-message',
-			'section' => AbstractEventRegistrationSpecialPage::DETAILS_SECTION
+			'section' => AbstractEventRegistrationSpecialPage::DETAILS_SECTION,
+			'validation-callback' => static function ( $grantID, $alldata ) {
+				$pattern = "/^\d+-\d+$/";
+				if ( preg_match( $pattern, $grantID ) || $grantID === '' ) {
+					return StatusValue::newGood();
+				}
+
+				return StatusValue::newFatal( 'wikimediacampaignevents-grant-id-invalid-error-message' );
+			},
 		];
 
+		return true;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onCampaignEventsRegistrationFormSubmit( array $formFields, int $eventID ): bool {
+		// TODO implementation
 		return true;
 	}
 }
