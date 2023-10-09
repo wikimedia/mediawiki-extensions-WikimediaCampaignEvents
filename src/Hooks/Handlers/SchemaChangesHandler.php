4ba412492d9eea1d3ad5c9c5f4a3abafa80f4308
Claudio Melo <cmelo@wikimedia.org>
2023-10-09 09:29:56 -0300

    Add DB schema to store grant ID
    Bug: T348444
    Change-Id: I1555f729f2c1f51ce2618ed80b973dd7fc1e0f56

---
+++ src/Hooks/Handlers/SchemaChangesHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\WikimediaCampaignEvents\Hooks\Handlers;
+
+use DatabaseUpdater;
+use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
+
+class SchemaChangesHandler implements LoadExtensionSchemaUpdatesHook {
+	/**
+	 * @param DatabaseUpdater $updater
+	 */
+	public function onLoadExtensionSchemaUpdates( $updater ) {
+		// NOTE: This hook does not support DI.
+		global $wgCampaignEventsDatabaseCluster, $wgCampaignEventsDatabaseName;
+
+		// We only want to create the schema on the shared DB. However, that seems hard to do due to how the
+		// installer/updater works (also, it's apparently not totally safe to access services at this point).
+		if (
+			// These globals are not set when running install.php
+			( $wgCampaignEventsDatabaseCluster !== null && $wgCampaignEventsDatabaseName !== null ) &&
+			( $wgCampaignEventsDatabaseCluster !== false && $wgCampaignEventsDatabaseName !== false )
+		) {
+			return;
+		}
+
+		$dbType = $updater->getDB()->getType();
+		$dir = __DIR__ . "/../../../db_patches";
+
+		$updater->addExtensionTable(
+			'wikimedia_campaign_events_grant',
+			"$dir/$dbType/tables-generated.sql"
+		);
+	}
+}
