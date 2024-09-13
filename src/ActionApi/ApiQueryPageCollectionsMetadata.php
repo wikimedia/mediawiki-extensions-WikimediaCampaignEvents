9a1b10e42897df19887e3eb56d42cf980145e3c7
eamedina <eamedina@wikimedia.org>
2024-09-13 14:51:47 -0400

    Community-defined page collections: Add parser and API for metadata
    To properly support community-defined page collections, we also need
    to define and fetch the collection metadata for each such collection.
    These collection metadata should be available through an API, that
    will be used by the Recommendation API to fetch the collection metadata
    and serve them along with the recommended articles for translation.
    
    This patch adds the logic to support the definition of such collection
    metadata, through the usage of a custom HTML marker that will include
    the metadata as HTML attributes, like below:
    ```
    <page-collection
      name='My Page Collection'
      description='This is a page collection'
      source-language='en'
      target-languages='es|fr'
      end-date='2024-10-20'
    ></page-collection>
    ```
    
    More specifically, this patch:
    - Uses a parser hook to parse such HTML markers, extract the collection
    metadata from its HTML attributes and store them as extension data.
    - Adds a tracking category to the pages that include such HTML marker,
    so that we can easily find/query such pages.
    - Add ApiQueryPageCollectionsMetadata Action API module to expose these
    collection metadata for the requested pages (if they include such
    markers).
    
    Bug: T373132
    Change-Id: I5f4b0041a4d3281702f2005ee1485b49610f8554

---
+++ src/ActionApi/ApiQueryPageCollectionsMetadata.php
@@ -0,0 +1,129 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\WikimediaCampaignEvents\ActionApi;
+
+use ApiQuery;
+use ApiQueryBase;
+use MediaWiki\Content\TextContent;
+use MediaWiki\Extension\WikimediaCampaignEvents\Hooks\Handlers\PageCollectionHookHandler;
+use MediaWiki\Page\WikiPageFactory;
+use MediaWiki\Revision\RevisionRecord;
+use MediaWiki\Title\TitleFactory;
+use ParserFactory;
+use ParserOptions;
+use Wikimedia\ParamValidator\ParamValidator;
+
+/**
+ * This action allows users to retrieve page collections metadata
+ * from pages including the page-collection marker, via the API.
+ *
+ * @ingroup API
+ */
+class ApiQueryPageCollectionsMetadata extends ApiQueryBase {
+	private ParserFactory $parserFactory;
+
+	private WikiPageFactory $wikiPageFactory;
+
+	private TitleFactory $titleFactory;
+
+	/**
+	 * @param ApiQuery $query
+	 * @param string $moduleName
+	 * @param ParserFactory $parserFactory
+	 * @param WikiPageFactory $wikiPageFactory
+	 * @param TitleFactory $titleFactory
+	 */
+	public function __construct(
+		$query,
+		$moduleName,
+		ParserFactory $parserFactory,
+		WikiPageFactory $wikiPageFactory,
+		TitleFactory $titleFactory
+	) {
+		parent::__construct( $query, $moduleName );
+		$this->parserFactory = $parserFactory;
+		$this->wikiPageFactory = $wikiPageFactory;
+		$this->titleFactory = $titleFactory;
+	}
+
+	public function execute() {
+		$this->run();
+	}
+
+	/**
+	 * Retrieves metadata for a given page collection, based on the page title.
+	 *
+	 * @param string $pageTitle The title of the page that includes a page-collection marker
+	 * @return array An associative array containing the page collection metadata.
+	 */
+	public function getPageCollectionMetadata( string $pageTitle ): array {
+		$parser = $this->parserFactory->getInstance();
+
+		$parserOptions = new ParserOptions( $this->getUser() );
+		$titleObj = $this->titleFactory->newFromText( $pageTitle );
+		if ( !$titleObj ) {
+			return [];
+		}
+		$wikiPage = $this->wikiPageFactory->newFromTitle( $titleObj );
+		$content = $wikiPage->getContent( RevisionRecord::RAW );
+
+		if ( $content instanceof TextContent ) {
+			$output = $parser->parse( $content->getText(), $titleObj, $parserOptions );
+			$metadata  = $output->getExtensionData( PageCollectionHookHandler::PAGE_COLLECTION_EXTENSION_DATA_KEY );
+		} else {
+			return [];
+		}
+
+		if ( !$metadata ) {
+			return [];
+		}
+
+		return \json_decode( $metadata, true );
+	}
+
+	private function run() {
+		$params = $this->extractRequestParams();
+
+		$pageTitles = array_map(
+			static function ( $pageTitle ) {
+				return urldecode( $pageTitle );
+			},
+			$params['titles']
+		);
+		$result = $this->getResult();
+
+		foreach ( $pageTitles as $pageTitle ) {
+			$metadata = $this->getPageCollectionMetadata( $pageTitle );
+			$result->addValue( [ 'query', 'page_collections' ], $pageTitle, $metadata );
+		}
+	}
+
+	/**
+	 * Returns the allowed parameters for the API query.
+	 *
+	 * @return array The array of allowed parameters.
+	 */
+	public function getAllowedParams() {
+		return [
+			'titles' => [
+				ParamValidator::PARAM_TYPE => 'string',
+				ParamValidator::PARAM_ISMULTI => true,
+				ParamValidator::PARAM_REQUIRED => true,
+			]
+		];
+	}
+
+	/**
+	 * Returns usage examples for this module.
+	 *
+	 * @return array The array of example messages.
+	 */
+	protected function getExamplesMessages() {
+		return [
+			'action=query&list=pagecollectionsmetadata&titles=TestCampaign'
+				=> 'apihelp-query+pagecollectionsmetadata-example-1',
+		];
+	}
+}
---
+++ src/Hooks/Handlers/PageCollectionHookHandler.php
@@ -0,0 +1,77 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\WikimediaCampaignEvents\Hooks\Handlers;
+
+use MediaWiki\Hook\ParserFirstCallInitHook;
+use MediaWiki\Parser\Parser;
+
+class PageCollectionHookHandler implements ParserFirstCallInitHook {
+	public const PAGE_COLLECTION_EXTENSION_DATA_KEY = 'PageCollection';
+
+	/**
+	 * Bind the parsePageCollection function to the page-collection magic word
+	 * @param Parser $parser
+	 */
+	public function onParserFirstCallInit( $parser ) {
+		$parser->setHook( "page-collection", [ $this, "parsePageCollection" ] );
+	}
+
+	/**
+	 * This method parses the custom <page-collection> HTML marker, extracts
+	 * the page collection metadata that are defined inside its attributes and stores them
+	 * as extension data, to be used by the "pagecollectionsmetadata" Action API module.
+	 *
+	 * It also adds a tracking category to the page that includes the marker, so that
+	 * we can easily find/query such pages.
+	 *
+	 * Finally, it returns an empty string, as we don't want to render anything inside
+	 * the page, for this marker.
+	 *
+	 * Example HTML marker definition:
+	 * <page-collection
+	 *   name='My Page Collection'
+	 *   description='This is a page collection'
+	 *   source-language='en'
+	 *   target-languages='es|fr'
+	 *   end-date='2024-10-20'
+	 * ></page-collection>
+	 *
+	 * @param string|null $label
+	 * @param array $args
+	 * @param Parser $parser
+	 * @return string
+	 */
+	public function parsePageCollection( ?string $label, array $args, Parser $parser ): string {
+		$parserOutput = $parser->getOutput();
+
+		// add a tracking category to the page that includes the marker, so that we can easily
+		// find/query such pages
+		$parser->addTrackingCategory( "page-collection-tracking-category" );
+
+		// Get the named parameters and merge with defaults.
+		$defaultOptions = [
+			"lang" => $parser->getContentLanguage()->getCode(),
+			"name" => "",
+			"description" => "",
+			"source-language" => "en",
+			"target-languages" => "",
+			"status" => "active",
+			"end-date" => "",
+		];
+		$pageCollectionDefinition = array_merge( $defaultOptions, $args );
+		$pageCollectionDefinition["target-languages"] = explode(
+			"|",
+			$pageCollectionDefinition["target-languages"]
+		);
+
+		$parserOutput->setExtensionData(
+			self::PAGE_COLLECTION_EXTENSION_DATA_KEY,
+			json_encode( $pageCollectionDefinition )
+		);
+
+		// For now, we don't want to display anything inside the page, for these HTML markers.
+		return "";
+	}
+}
