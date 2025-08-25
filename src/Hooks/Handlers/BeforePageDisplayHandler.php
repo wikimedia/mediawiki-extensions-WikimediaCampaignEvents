caf7aeb5d86857ba8964196ccc0a493a9231f422
Huei Tan <htan@wikimedia.org>
2025-08-25 16:09:16 +0200

    Setup tracking for CentralNotice banners experiment for WE2.1.1
    Tracking code for Scenarios 1 for WE2.1.1
    
    Bug: T402496
    Change-Id: Icbac75d4370290f2acb8c8d96caf547b947392bf

---
+++ src/Hooks/Handlers/BeforePageDisplayHandler.php
@@ -0,0 +1,71 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\WikimediaCampaignEvents\Hooks\Handlers;
+
+use MediaWiki\Output\Hook\BeforePageDisplayHook;
+use MediaWiki\Output\OutputPage;
+use MediaWiki\SpecialPage\SpecialPageFactory;
+
+/**
+ * FY25 WE2.1.1 Hook handler
+ *
+ * Modifies Special:HomePage links to include source tracking.
+ *
+ * Read more https://phabricator.wikimedia.org/T402496
+ */
+class BeforePageDisplayHandler implements BeforePageDisplayHook {
+
+	private const EXPERIMENT_PAGES = [
+		'Language_Experiment_2025',
+		'RANCANGAN_TES_ACARA',
+		'نضم_لينا!'
+	];
+
+	private SpecialPageFactory $specialPageFactory;
+
+	public function __construct( SpecialPageFactory $specialPageFactory ) {
+		$this->specialPageFactory = $specialPageFactory;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onBeforePageDisplay( $out, $skin ): void {
+		// Check if conditions are met to load the FY25 WE2.1.1 module
+		if ( $this->shouldLoadModule( $out ) ) {
+			$out->addModules( 'ext.campaignEvents.fy25-we211' );
+			$out->addJsConfigVars(
+				'wgSpecialHomepageLocalName', $this->specialPageFactory->getLocalNameFor( 'Homepage' )
+			);
+		}
+	}
+
+	/**
+	 * Check if the module should be loaded for this page.
+	 *
+	 * @param OutputPage $out The OutputPage object
+	 * @return bool True if the module should be loaded, false otherwise
+	 */
+	private function shouldLoadModule( OutputPage $out ): bool {
+		$title = $out->getTitle();
+		if ( $title->getNamespace() !== NS_EVENT ) {
+			return false;
+		}
+
+		if ( !in_array( $title->getDBkey(), self::EXPERIMENT_PAGES, true ) ) {
+			return false;
+		}
+
+		if ( $out->getContext()->getActionName() !== 'view' ) {
+			return false;
+		}
+
+		if ( $out->getRequest()->getVal( 'wprov' ) !== 'cbeer' ) {
+			return false;
+		}
+
+		return true;
+	}
+}
